053ba8c6283e9935ae35241009bd6e51d0b076e1
MuhammetEminAlper <eminalpernl@gmail.com>
2025-03-16 14:10:43 +0100

    LINQ

--- adventofcode_day1-1/Program.cs
+++ adventofcode_day1-1/Program.cs
@@ -1,28 +1,62 @@
 ﻿
-string filePath = @"filePath.txt";
-string[] lines = File.ReadAllLines(filePath);
+//string filePath = @"filePath.txt";
+//string[] lines = File.ReadAllLines(filePath);
 
-List<int> firstNumbers = new List<int>();
-List<int> secondNumbers = new List<int>();
+//List<int> firstNumbers = new List<int>();
+//List<int> secondNumbers = new List<int>();
 
-foreach (string line in lines)
+//foreach (string line in lines)
+//{
+//    string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+//    if (parts.Length == 2 && int.TryParse(parts[0], out int firstNumber) && int.TryParse(parts[1], out int secondNumber))
+//    {
+//        firstNumbers.Add(firstNumber);
+//        secondNumbers.Add(secondNumber);
+//    }
+//}
+
+//firstNumbers.Sort();
+//secondNumbers.Sort();
+
+//int totaalDifference = 0;
+
+//for (int i = 0; i < firstNumbers.Count; i++)
+//{
+//    totaalDifference += Math.Abs(firstNumbers[i] - secondNumbers[i]);
+//}
+
+//Console.WriteLine(totaalDifference);
+
+
+
+
+//////////////////////////////////     LINQ     //////////////////////////////////////
+///
+
+
+using System;
+using System.IO;
+using System.Linq;
+
+class Program
 {
-    string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
-    if (parts.Length == 2 && int.TryParse(parts[0], out int firstNumber) && int.TryParse(parts[1], out int secondNumber))
+    static void Main()
     {
-        firstNumbers.Add(firstNumber);
-        secondNumbers.Add(secondNumber);
+        string filePath = "filePath.txt";
+
+        var numberPairs = File.ReadLines(filePath)
+                          .Where(s => !string.IsNullOrWhiteSpace(s)) 
+                          .Select(s => s.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                        .Select(int.Parse).ToArray())
+                          .Where(a => a.Length == 2)
+                          .ToList();
+
+
+        var firstNumbers = numberPairs.Select(a => a[0]).OrderBy(x => x).ToArray();
+        var secondNumbers = numberPairs.Select(a => a[1]).OrderBy(x => x).ToArray();
+
+        int totalDifference = firstNumbers.Zip(secondNumbers, (a, b) => Math.Abs(a - b)).Sum();
+
+        Console.WriteLine("Totaal abs: " + totalDifference);
     }
 }
-
-firstNumbers.Sort();
-secondNumbers.Sort();
-
-int totaalDifference = 0;
-
-for (int i = 0; i < firstNumbers.Count; i++)
-{
-    totaalDifference += Math.Abs(firstNumbers[i] - secondNumbers[i]);
-}
-
-Console.WriteLine(totaalDifference);
