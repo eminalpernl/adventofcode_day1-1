80b5d60872610e9315d032aa8f2f2873e0e1c632
MuhammetEminAlper <eminalpernl@gmail.com>
2025-03-16 14:02:05 +0100

    day1-1

--- adventofcode_day1-1/Program.cs
+++ adventofcode_day1-1/Program.cs
@@ -1,1 +1,28 @@
 ﻿
+string filePath = @"filePath.txt";
+string[] lines = File.ReadAllLines(filePath);
+
+List<int> firstNumbers = new List<int>();
+List<int> secondNumbers = new List<int>();
+
+foreach (string line in lines)
+{
+    string[] parts = line.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+    if (parts.Length == 2 && int.TryParse(parts[0], out int firstNumber) && int.TryParse(parts[1], out int secondNumber))
+    {
+        firstNumbers.Add(firstNumber);
+        secondNumbers.Add(secondNumber);
+    }
+}
+
+firstNumbers.Sort();
+secondNumbers.Sort();
+
+int totaalDifference = 0;
+
+for (int i = 0; i < firstNumbers.Count; i++)
+{
+    totaalDifference += Math.Abs(firstNumbers[i] - secondNumbers[i]);
+}
+
+Console.WriteLine(totaalDifference);
