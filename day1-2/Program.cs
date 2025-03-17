307399519376022ab68423a2c02984d010e93572
MuhammetEminAlper <eminalpernl@gmail.com>
2025-03-17 22:55:32 +0100

    day1-2

---
+++ day1-2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+class Program
+{
+    static void Main()
+    {
+        string filePath = "filePath.txt";
+        List<int> quantity = new List<int>();
+        var numberPairs = File.ReadLines(filePath)
+                          .Where(s => !string.IsNullOrWhiteSpace(s))
+                          .Select(s => s.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                                        .Select(int.Parse).ToArray())
+                          .Where(a => a.Length == 2)
+                          .ToList();
+
+
+        int[] firstNumbers = numberPairs.Select(a => a[0]).ToArray();
+        int[] secondNumbers = numberPairs.Select(a => a[1]).ToArray();
+
+        int totaal = firstNumbers.Sum(x => x * secondNumbers.Count(y => y == x));
+
+        //int totalDifference = firstNumbers.Zip(secondNumbers, (a, b) => Math.Abs(a - b)).Sum();
+
+        Console.WriteLine("Totaal: " + totaal);
+
+    }
+}
